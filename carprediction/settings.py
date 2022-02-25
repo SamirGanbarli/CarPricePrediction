5814b5918f984b4835c220e4598d69c30bb09138
Samir Ganbarli <70481407+SamirGanbarli@users.noreply.github.com>
2022-02-26 01:08:34 +0300

    Update settings.py

--- carprediction/settings.py
+++ carprediction/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vu!!i8gzc3$a2y(i@f6d+*l)57^na-60s56-!i=o+tvu-%28k3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
